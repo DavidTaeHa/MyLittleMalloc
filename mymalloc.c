70f761b5fd6fc85ee902a392b8ef8a58d9aa2529
Gandhi115 <61490541+Gandhi115@users.noreply.github.com>
2022-02-15 01:28:38 -0500

    Start Project
    Created the start of the project

---
+++ mymalloc.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "mymalloc.h"
+
+static char memory[4096];
+
+void *mymalloc(size_t size, char *file, int line)
+{
+
+}
+
+void myfree(void *ptr, char *file, int line)
+{
+
+}
