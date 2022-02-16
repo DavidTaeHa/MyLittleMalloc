66de3c1da2a5e5a63bb2c444f097592ff3c16156
Artiomis <99773395+Artiomis@users.noreply.github.com>
2022-02-15 20:23:22 -0500

    Update mymalloc.h

--- mymalloc.h
+++ mymalloc.h
@@ -1,8 +1,18 @@
+#ifndef MYMALLOC_H_
+#define MYMALLOC_H_)
 #include <stdio.h>
 #include <stdlib.h>
+#define malloc(s) mymalloc(s, __FILE__, __LINE__);
+#define free(p) myfree(p, __FILE__, __LINE__);
 
+typedef struct{
+  unsigned int BlockSz;
+  unsigned int preBlockSz;
+} node;
+
+node *prevPtr(node *);
+node *nextPtr(node *);
 void *mymalloc(size_t size, char *file, int line);
 void myfree(void *ptr, char *file, int line);
 
-#define malloc(s) mymalloc(s, __FILE__, __LINE__);
-#define free(p) myfree(p, __FILE__, __LINE__);
+#endif
