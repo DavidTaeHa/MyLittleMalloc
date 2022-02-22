ceca31a0330b6c64dae096f7ce183a57e4148e71
Artiomis <99773395+Artiomis@users.noreply.github.com>
2022-02-22 06:38:22 -0500

    Update memgrind.c

--- memgrind.c
+++ memgrind.c
@@ -1,95 +1,94 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<math.h>
-#include<sys/time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <sys/time.h>
 #include "mymalloc.h"
 
 /*Malloc and immediately free 1-byte chunk, 120 times*/
 double grindOne(){
-  int i;
-  struct timeval tval1, tval2;
-  void *arr[200];
+    int i;
+    struct timeval tval1, tval2;
+    void *arr[200];
   
-  gettimeofday(&tval1, NULL);
-  for(i = 0; i < 120; i++){
-    arr[i] = malloc(1);
-    free(arr[i]);
-  }
-  gettimeofday(&tval2, NULL);
+    gettimeofday(&tval1, NULL);
+    for(i = 0; i < 120; i++){
+        arr[i] = malloc(1);
+        free(arr[i]);
+    }
+    gettimeofday(&tval2, NULL);
   
-  double totalTime = (double)(tval2.tv_sec - tval1.tv_sec) + (double)(tval2.tv_usec - tval1.tv_usec)/1000000;
-  return totalTime;
+    double totalTime = (double)(tval2.tv_sec - tval1.tv_sec) + (double)(tval2.tv_usec - tval1.tv_usec)/1000000;
+    return totalTime;
 }
 
 /*Malloc 120 1-byte chunks, storing pointers in array, then use free to deallocate the chunks*/
 double grindTwo(){
-  int i;
-  struct timeval tval1, tval2;
-  void *arr[200];
+    int i;
+    struct timeval tval1, tval2;
+    void *arr[200];
   
-  gettimeofday(&tval1, NULL);
-  for(i = 0; i < 120; i++){
-    arr[i] = malloc(1);
-  }
-  for(i = 0; i < 120; i++){
-    free(arr[i]);
-  }
-  gettimeofday(&tval2, NULL);
+    gettimeofday(&tval1, NULL);
+    for(i = 0; i < 120; i++){
+        arr[i] = malloc(1);
+    }
+    for(i = 0; i < 120; i++){
+        free(arr[i]);
+    }
+    gettimeofday(&tval2, NULL);
   
-  double totalTime = (double)(tval2.tv_sec - tval1.tv_sec) + (double)(tval2.tv_usec - tval1.tv_usec)/1000000;
-  return totalTime;
+    double totalTime = (double)(tval2.tv_sec - tval1.tv_sec) + (double)(tval2.tv_usec - tval1.tv_usec)/1000000;
+    return totalTime;
 }
 
 /*Randomly choose to malloc or free a 1-byte chunk. Perform until malloc is used 120 times, then free all remaining chunks*/
 double grindThree(){
-  int i;
-  struct timeval tval1, tval2;
-  void *arr[200];
-  int count = 0;
-  int size = 0;
-  int random = 0;
+    struct timeval tval1, tval2;
+    void *arr[200];
+    int count = 0;
+    int size = 0;
+    int random = 0;
   
-  gettimeofday(&tval1, NULL);
-  while(count < 120){
-    random = (rand() % 2);
+    gettimeofday(&tval1, NULL);
+    while(count < 120){
+        random = (rand() % 2);
     
-    if(random == 0 || size == -1){
-      arr[size] = malloc(1);
-      count++;
-      size++;
+        if(random == 0 || size == -1){
+            arr[size] = malloc(1);
+            count++;
+            size++;
+        }
+        else{
+            free(arr[size-1]);
+            size--;
+        }
     }
-    else{
-      free(arr[size-1]);
-      size--;
+    while(size > 0){
+          free(arr[size-1]);
+          size--;
     }
-  }
-  if(size > 0){
-    for(size; size > 0; size--){
-      free(arr[size-1]);
-    }
-  }
-  gettimeofday(&tval2, NULL);
+    gettimeofday(&tval2, NULL);
   
-  double totalTime = (double)(tval2.tv_sec - tval1.tv_sec) + (double)(tval2.tv_usec - tval1.tv_usec)/1000000;
-  return totalTime;
+    double totalTime = (double)(tval2.tv_sec - tval1.tv_sec) + (double)(tval2.tv_usec - tval1.tv_usec)/1000000;
+    return totalTime;
 }
 
 
 
 int main(){
-  int i;
-  double first = 0;
-  double second = 0;
-  double third = 0;
-  //double fourth = 0;
-  //double fifth = 0;
-
-  for(i=0; i<50; i++){
-    first += grindOne();
-    second += grindTwo();
-    third += grindThree();
-  }
-  printf("Average of Immediate: %f seconds\n", first/50);
-  printf("Average of Delayed: %f seconds\n", second/50);
-  printf("Average of Random: %f seconds\n", third/50);
+    int i;
+    double first = 0;
+    double second = 0;
+    double third = 0;
+    //double fourth = 0;
+    //double fifth = 0;
+  
+    /*Malloc and Free 1-byte chunk, 120 times*/
+    for(i=0; i<50; i++){
+        first += grindOne();
+        second += grindTwo();
+        third += grindThree();
+    }
+    printf("Average of Immediate: %f seconds\n", first/50);
+    printf("Average of Delayed: %f seconds\n", second/50);
+    printf("Average of Random: %f seconds\n", third/50);
 }
