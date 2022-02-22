250eeaac97a9b8b8ffffd8c04765f8830455ad5a
Gandhi115 <61490541+Gandhi115@users.noreply.github.com>
2022-02-22 14:35:46 -0500

    update

--- memgrind.c
+++ memgrind.c
@@ -8,12 +8,13 @@
 double grindOne(){
     int i;
     struct timeval tval1, tval2;
-    void *arr[200];
+    int *arr[200];
   
     gettimeofday(&tval1, NULL);
     for(i = 0; i < 120; i++){
-        arr[i] = malloc(1);
+        arr[i] = (int *) malloc(1);
         free(arr[i]);
+        arr[i] = NULL;
     }
     gettimeofday(&tval2, NULL);
   
@@ -25,14 +26,15 @@ double grindOne(){
 double grindTwo(){
     int i;
     struct timeval tval1, tval2;
-    void *arr[200];
+    int *arr[200];
   
     gettimeofday(&tval1, NULL);
     for(i = 0; i < 120; i++){
-        arr[i] = malloc(1);
+        arr[i] = (int *) malloc(1);
     }
     for(i = 0; i < 120; i++){
         free(arr[i]);
+        arr[i] = NULL;
     }
     gettimeofday(&tval2, NULL);
   
@@ -43,28 +45,36 @@ double grindTwo(){
 /*Randomly choose to malloc or free a 1-byte chunk. Perform until malloc is used 120 times, then free all remaining chunks*/
 double grindThree(){
     struct timeval tval1, tval2;
-    void *arr[200];
+    int *arr[1000];
     int count = 0;
     int size = 0;
+    int fcount = 0;
     int random = 0;
   
     gettimeofday(&tval1, NULL);
     while(count < 120){
-        random = (rand() % 2);
-    
-        if(random == 0 || size == -1){
-            arr[size] = malloc(1);
+        random = rand() % 2;
+        if(random == 0 && count < 120){
+            arr[size] = (int *) malloc(1);
             count++;
             size++;
         }
         else{
-            free(arr[size]);
-            size--;
+            if(arr[fcount] == NULL){
+                arr[size] = (int *) malloc(1);
+                size++;
+                count++;
+            }
+            else{
+                free(arr[fcount]);
+                arr[fcount] = NULL;
+                fcount++;
+            }
         }
     }
-    while(size > 0){
-          free(arr[size-1]);
-          size--;
+    for(size = fcount; size < 120; size++){
+        free(arr[size]);
+	    arr[size] = NULL;
     }
     gettimeofday(&tval2, NULL);
   
@@ -82,13 +92,22 @@ int main(){
     //double fourth = 0;
     //double fifth = 0;
   
-    /*Malloc and Free 1-byte chunk, 120 times*/
-    for(i=0; i<50; i++){
+    for(i = 0; i < 50; i++){
         first += grindOne();
+    }
+    for(i = 0; i < 50; i++){
         second += grindTwo();
+    }
+    for(i = 0; i < 50; i++){
         third += grindThree();
     }
+    printf("Total of Immediate: %f seconds\n", first);
+    printf("Total of Delayed: %f seconds\n", second);
+    printf("Total of Random: %f seconds\n", third);
+    printf("\n");
     printf("Average of Immediate: %f seconds\n", first/50);
     printf("Average of Delayed: %f seconds\n", second/50);
     printf("Average of Random: %f seconds\n", third/50);
+
+    return 0;
 }
